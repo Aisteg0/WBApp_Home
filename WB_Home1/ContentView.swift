86153ac289d1d1a226902f67f0cdfbe5f5b5c08a
Misha <Mishka.ganin@mail.ru>
2024-06-08 19:28:54 +0300

    Init

---
+++ WB_Home1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WB_Home1
+//
+//  Created by Михаил Ганин on 08.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WB_Home1/WB_Home1App.swift
@@ -0,0 +1,17 @@
+//
+//  WB_Home1App.swift
+//  WB_Home1
+//
+//  Created by Михаил Ганин on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB_Home1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
