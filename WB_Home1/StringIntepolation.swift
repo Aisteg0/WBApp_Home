c80cfc166e217e77ea8bc531de6f14aa74d8c0bb
Misha <Mishka.ganin@mail.ru>
2024-06-25 23:45:55 +0300

    init

---
+++ WB_Home1/StringIntepolation.swift
@@ -0,0 +1,18 @@
+//
+//  StringIntepolation.swift
+//  WB_Home1
+//
+//  Created by Михаил Ганин on 25.06.2024.
+//
+
+import SwiftUI
+
+struct StringIntepolation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StringIntepolation()
+}
