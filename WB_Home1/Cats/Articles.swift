6fb7652a55c4318053161e9abdafbf8a6fb1084c
Misha <Mishka.ganin@mail.ru>
2024-08-07 17:43:55 +0300

    add commit about loading website

--- WB_Home1/Cats/Articles.swift
+++ WB_Home1/Cats/Articles.swift
@@ -15,6 +15,7 @@ struct Articles {
     init() {}
     
     mutating func loadCats(completion: @escaping (_ data: URL?, _ error: Error?) -> Void) {
+        // Там иногда сайт этих котов лежит и они не грузятся, но работает все норм, когда сайт не лежит, если надо, могу видосик прислать)
         CatsAPI.catGet(id: "cat") { data, error in
             completion(data, error)
         }
